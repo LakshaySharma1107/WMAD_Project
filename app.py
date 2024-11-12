ee59eedb4935aea8c07b389c8d012c2a2ce57f26
LakshaySharma1107 <147788900+LakshaySharma1107@users.noreply.github.com>
2024-11-12 22:53:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -230,4 +230,5 @@ def download_audio(audio_file_name):
         return jsonify({"message": "Error during file download"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(debug=True, host="0.0.0.0", port=port)
