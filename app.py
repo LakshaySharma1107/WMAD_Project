45ac74d96dd539be874fc3921022b4ca9d66a701
LakshaySharma1107 <147788900+LakshaySharma1107@users.noreply.github.com>
2024-11-12 19:52:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -259,5 +259,6 @@ def process_audio_route():
         print(f"Error in /process_audio route: {e}")
         return jsonify({"error": "Internal Server Error"}), 500
 
-if _name_ == "_main_":
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(debug=True, host="0.0.0.0", port=port)
